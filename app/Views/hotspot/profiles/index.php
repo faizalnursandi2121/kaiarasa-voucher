e8509949379a386e6c4ead8b4e94ecdd462784a8
faizaldev <200047661+faizalnursandi2121@users.noreply.github.com>
2026-08-28 14:00:23 +0700

    fix(profiles): perkuat CSS hide spinner (display:none + appearance:none)
    Tambah display:none dan standard 'appearance: none' (selain
    -webkit-appearance) untuk memastikan spinner benar-benar hilang di
    semua browser (Chrome/Safari/Firefox). Sebelumnya sudah ada
    -webkit-appearance: none tapi beberapa browser masih render
    pseudo-element inline-block.
    
    Computed style verification:
    - input[type=number]::-webkit-inner-spin-button: appearance: none
    - input[type=number]::-webkit-outer-spin-button: appearance: none
    - input[type=number]: appearance: none (sebelumnya textfield)

--- app/Views/hotspot/profiles/index.php
+++ app/Views/hotspot/profiles/index.php
@@ -5,11 +5,14 @@ $title = 'Access Packages'; ?>
 input[type="number"]::-webkit-inner-spin-button,
 input[type="number"]::-webkit-outer-spin-button {
     -webkit-appearance: none;
+            appearance: none;
     margin: 0;
+    display: none;
 }
 input[type="number"] {
     -moz-appearance: textfield;
     appearance: textfield;
+    -webkit-appearance: none;
 }
 </style>
 <?php
